ff1732e5c97872a1b907042301cbd6757d830c79
Glaucoed <gesverzuthe@gmail.com>
2024-05-19 12:25:46 -0300

    fix: resetado repsonsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,64 +1,34 @@
-"use client";
+
+"use client"
 import Image from "next/image";
-import { EffectFlip, Pagination, Navigation } from "swiper/modules";
-import "swiper/css";
-import "swiper/css/effect-flip";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
-import { Swiper, SwiperSlide } from "swiper/react";
+import { EffectFlip, Pagination, Navigation } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/effect-flip';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import { Swiper, SwiperSlide } from 'swiper/react';
 import ImagemLoader from "../components/ImageLazyLoading";
-import manicure1 from "../../public/img1.jpg";
-import manicure2 from "../../public/img2.jpg";
-import manicure4 from "../../public/img4.jpg";
-import manicure5 from "../../public/img5.jpg";
-import manicure6 from "../../public/img6.jpg";
-import manicure7 from "../../public/img7.jpg";
-import manicure8 from "../../public/img8.jpg";
-import manicure9 from "../../public/img9.jpg";
-import manicure10 from "../../public/img10.jpg";
-import manicure11 from "../../public/img11.jpg";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col w-full pt-20 gap-10">
       <div className="flex flex-col justify-center items-center gap-4">
-        <ImagemLoader />
-        <p className="text-4xl font-bold text-white text-center">
-          Tailine Nails Designer
-        </p>
-        <h1 className="text-azul-medio px-10">
-          {" "}
-          Alongamento de fibra • Esmaltação em Gel • Manicure • Pedicure • Spa
-          dos pés
-        </h1>
+        < ImagemLoader />
+        <p className="text-4xl font-bold text-white text-center">Tailine Nails Designer</p>
+        <h1 className="text-azul-medio px-10"> Alongamento de fibra • Esmaltação em Gel • Manicure • Pedicure • Spa dos pés</h1>
         <h2 className="text-lg px-10 lg:px-0 text-white">
           Unhas que brilham tanto quanto sua personalidade ✨
         </h2>
       </div>
 
       <div className="flex flex-col lg:w-1/2 w-full lg:px-0 px-10 mx-auto gap-2">
-        <a
-          title="WhatsApp"
-          target="_blank"
-          href="https://api.whatsapp.com/send/?phone=5511913512961&text=Tenho+interesse+em+agendar+um+horario&type=phone_number&app_absent=0"
-          className="bg-[#022B3A] hover:bg-[#1F7A8C] text-[#BFDBF7] font-bold py-3 px-4 border-b-4 border-[#BFDBF7] hover:border-[#BFDBF7] rounded text-center"
-        >
+        <a title="WhatsApp" target="_blank" href="https://api.whatsapp.com/send/?phone=5511913512961&text=Tenho+interesse+em+agendar+um+horario&type=phone_number&app_absent=0" className="bg-[#022B3A] hover:bg-[#1F7A8C] text-[#BFDBF7] font-bold py-3 px-4 border-b-4 border-[#BFDBF7] hover:border-[#BFDBF7] rounded text-center">
           WhatsApp
         </a>
-        <a
-          title="Localização"
-          target="_blank"
-          href="https://g.co/kgs/3qe1Cde"
-          className="bg-[#022B3A] hover:bg-[#1F7A8C] text-[#BFDBF7] font-bold py-3 px-4 border-b-4 border-[#BFDBF7] hover:border-[#BFDBF7] rounded text-center"
-        >
+        <a title="Localização" target="_blank" href="https://g.co/kgs/3qe1Cde" className="bg-[#022B3A] hover:bg-[#1F7A8C] text-[#BFDBF7] font-bold py-3 px-4 border-b-4 border-[#BFDBF7] hover:border-[#BFDBF7] rounded text-center">
           Localização
         </a>
-        <a
-          title="Instragram"
-          target="_blank"
-          href="https://www.instagram.com/tai_esverzuthenails/"
-          className="bg-[#022B3A] hover:bg-[#1F7A8C] text-[#BFDBF7] font-bold py-3 px-4 border-b-4 border-[#BFDBF7] hover:border-[#BFDBF7] rounded text-center"
-        >
+        <a title="Instragram" target="_blank" href="https://www.instagram.com/tai_esverzuthenails/" className="bg-[#022B3A] hover:bg-[#1F7A8C] text-[#BFDBF7] font-bold py-3 px-4 border-b-4 border-[#BFDBF7] hover:border-[#BFDBF7] rounded text-center">
           Instagram
         </a>
       </div>
@@ -132,6 +102,7 @@ export default function Home() {
               Pedicure Comum
             </h3>
             <div className="flex gap-3 mt-2 items-center">
+
               <span className="font-bold text-lg text-azul-escuro">
                 R$ 40,00
               </span>
@@ -140,139 +111,54 @@ export default function Home() {
         </div>
       </div>
       <div className="lg:w-[600px] w-full mx-auto">
-        <h3 className="font-bold text-2xl text-center text-azul-escuro pt-10 pb-5 uppercase">
+      <h3 className="font-bold text-2xl text-center text-azul-escuro pt-10 pb-5 uppercase">
           Meus Trabalhos
         </h3>
-        <Swiper
-          effect={"flip"}
-          loop={true}
-          grabCursor={true}
-          pagination={true}
-          navigation={true}
-          modules={[EffectFlip, Pagination, Navigation]}
-          className="!bg-white !rounded !p-10 !mx-10"
-        >
-          <SwiperSlide className="lg:max-w-[600px] max-w-full">
-            <Image
-              src={manicure1}
-              alt="manicure"
-              sizes="100vw"
-              style={{
-                width: "100%",
-                height: "auto",
-              }}
-            />
-          </SwiperSlide>
-          <SwiperSlide className="lg:max-w-[600px] max-w-full">
-            <Image
-              src={manicure2}
-              alt="manicure"
-              sizes="100vw"
-              style={{
-                width: "100%",
-                height: "auto",
-              }}
-            />
-          </SwiperSlide>
-          <SwiperSlide className="lg:max-w-[600px] max-w-full">
-            <Image
-              src={manicure4}
-              alt="manicure"
-              sizes="100vw"
-              style={{
-                width: "100%",
-                height: "auto",
-              }}
-            />
-          </SwiperSlide>
-          <SwiperSlide className="lg:max-w-[600px] max-w-full">
-            <Image
-              src={manicure5}
-              alt="manicure"
-              sizes="100vw"
-              style={{
-                width: "100%",
-                height: "auto",
-              }}
-            />
-          </SwiperSlide>
-          <SwiperSlide className="lg:max-w-[600px] max-w-full">
-            <Image
-              src={manicure6}
-              alt="manicure"
-              sizes="100vw"
-              style={{
-                width: "100%",
-                height: "auto",
-              }}
-            />
-          </SwiperSlide>
-          <SwiperSlide className="lg:max-w-[600px] max-w-full">
-            <Image
-              src={manicure7}
-              alt="manicure"
-              sizes="100vw"
-              style={{
-                width: "100%",
-                height: "auto",
-              }}
-            />
-          </SwiperSlide>
-          <SwiperSlide className="lg:max-w-[600px] max-w-full">
-            <Image
-              src={manicure8}
-              alt="manicure"
-              sizes="100vw"
-              style={{
-                width: "100%",
-                height: "auto",
-              }}
-            />
-          </SwiperSlide>
-          <SwiperSlide className="lg:max-w-[600px] max-w-full">
-            <Image
-              src={manicure9}
-              alt="manicure"
-              sizes="100vw"
-              style={{
-                width: "100%",
-                height: "auto",
-              }}
-            />
-          </SwiperSlide>
-          <SwiperSlide className="lg:max-w-[600px] max-w-full">
-            <Image
-              src={manicure10}
-              alt="manicure"
-              sizes="100vw"
-              style={{
-                width: "100%",
-                height: "auto",
-              }}
-            />
-          </SwiperSlide>
-          <SwiperSlide className="lg:max-w-[600px] max-w-full">
-            <Image
-              src={manicure11}
-              alt="manicure"
-              sizes="100vw"
-              style={{
-                width: "100%",
-                height: "auto",
-              }}
-            />
-          </SwiperSlide>
-        </Swiper>
+      <Swiper
+        effect={'flip'}
+        loop={true}
+        grabCursor={true}
+        pagination={true}
+        navigation={true}
+
+        modules={[EffectFlip, Pagination, Navigation]}
+        className="!bg-white !rounded !p-10 !mx-10"
+      >
+        <SwiperSlide className="lg:max-w-[600px]">
+          <Image src="/img1.jpg" alt="manicure" width={600} height={600} />
+        </SwiperSlide>
+        <SwiperSlide className="lg:max-w-[600px]">
+          <Image src="/img2.jpg" alt="manicure" width={600} height={600} />
+        </SwiperSlide>
+        <SwiperSlide className="lg:max-w-[600px]">
+          <Image src="/img4.jpg" alt="manicure" width={600} height={600} />
+        </SwiperSlide >
+        <SwiperSlide className="lg:max-w-[600px]">
+          <Image src="/img5.jpg" alt="manicure" width={600} height={600} />
+        </SwiperSlide >
+        <SwiperSlide className="lg:max-w-[600px]">
+          <Image src="/img6.jpg" alt="manicure" width={600} height={600} />
+        </SwiperSlide >
+        <SwiperSlide className="lg:max-w-[600px]">
+          <Image src="/img7.jpg" alt="manicure" width={600} height={600} />
+        </SwiperSlide>
+        <SwiperSlide className="lg:max-w-[600px]">
+          <Image src="/img8.jpg" alt="manicure" width={600} height={600} />
+        </SwiperSlide>
+        <SwiperSlide className="lg:max-w-[600px]">
+          <Image src="/img9.jpg" alt="manicure" width={600} height={600} />
+        </SwiperSlide>
+        <SwiperSlide className="lg:max-w-[600px]">
+          <Image src="/img10.jpg" alt="manicure" width={600} height={600} />
+        </SwiperSlide>
+        <SwiperSlide className="lg:max-w-[600px]">
+          <Image src="/img11.jpg" alt="manicure" width={600} height={600} />
+        </SwiperSlide>
+
+      </Swiper>
       </div>
       <div className="bg-azul-escuro flex justify-center items-center py-2 gap-2 text-azul-claro">
-        Desenvolvido por{" "}
-        <a
-          className="font-bold text-white"
-          target="_blank"
-          href="http://www.glaucoed.com "
-        >
-          Glauco
-        </a>
+        Desenvolvido por <a className="font-bold text-white" target="_blank" href="http://www.glaucoed.com ">Glauco</a>
       </div>
     </main>
   );
